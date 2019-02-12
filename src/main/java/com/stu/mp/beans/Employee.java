991ab95347b6e08935d6e11754ef0681dedc99db
wdx <wdx@qq.comc>
2019-02-12 15:03:58 +0800

    认为

---
+++ src/main/java/com/stu/mp/beans/Employee.java
@@ -0,0 +1,86 @@
+package com.stu.mp.beans;
+
+import java.io.Serializable;
+
+import com.baomidou.mybatisplus.activerecord.Model;
+import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.annotations.TableLogic;
+import com.baomidou.mybatisplus.annotations.TableName;
+import com.baomidou.mybatisplus.enums.IdType;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author wdx
+ * @since 2019-01-18
+ */
+public class Employee extends Model<Employee> {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableLogic
+    private Integer id;
+    private String lastName;
+    private String email;
+    private String gender;
+    private Integer age;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    protected Serializable pkVal() {
+        return this.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+        ", id=" + id +
+        ", lastName=" + lastName +
+        ", email=" + email +
+        ", gender=" + gender +
+        ", age=" + age +
+        "}";
+    }
+}
